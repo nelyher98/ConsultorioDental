5bfc30949d01122293c316178ce2f1b29c1f581c
xomh <mhxochitl@gmail.com>
2019-03-08 08:58:21 -0600

    Validación con js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-
+function go(){
+    
+    if(document.form.psw.value=='contraseña' && document.form.uname.value=='usuario'){
+        alert("Usuario y contraseña válidos");
+        window.location='Citas.html';
+    }else{
+        alert("Por favor, ingrese un usuario y contraseña");
+    }
+}
